1734a6495fd605e02c764b5e431f8d243bfc5f13
Mikkel Ricky <rimi@aarhus.dk>
2026-07-09 12:47:36 +0200

    Cleaned up repositories

--- src/Repository/ActionLogEntryRepository.php
+++ src/Repository/ActionLogEntryRepository.php
@@ -15,29 +15,4 @@ class ActionLogEntryRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, ActionLogEntry::class);
     }
-
-    //    /**
-    //     * @return ActionLogEntry[] Returns an array of ActionLogEntry objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('a')
-    //            ->andWhere('a.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('a.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?ActionLogEntry
-    //    {
-    //        return $this->createQueryBuilder('a')
-    //            ->andWhere('a.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
 }
