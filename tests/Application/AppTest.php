3b356c551be6c6c02c140575278254b2b3792ae2
Mikkel Ricky <rimi@aarhus.dk>
2026-08-04 15:18:11 +0200

    Cleaned up

--- tests/Application/AppTest.php
+++ tests/Application/AppTest.php
@@ -4,12 +4,13 @@ namespace App\Tests\Application;
 
 use App\Entity\User;
 use App\Repository\UserRepository;
+use App\Tests\ApplicationTestCaseTrait;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-use Symfony\Component\BrowserKit\Exception\InvalidArgumentException;
-use Symfony\Component\DomCrawler\Crawler;
 
 class AppTest extends WebTestCase
 {
+    use ApplicationTestCaseTrait;
+
     public function testSomething(): void
     {
         $client = static::createClient();
@@ -28,7 +29,7 @@ class AppTest extends WebTestCase
         $this->assertResponseIsSuccessful();
 
         $client->submitForm('Look up entry', [
-            'entry_look_up_form[identifier]' => '123-test',
+            'entry_look_up_form[identifier]' => '123-hest',
         ]);
         $this->assertResponseIsUnprocessable();
         $this->assertSelectorTextContains('*', 'Invalid identifier');
@@ -48,14 +49,13 @@ class AppTest extends WebTestCase
         $this->assertFormValue('form', 'entry_remove_form[department]', $user->getDepartments()->first()->getId(), 'User department selected');
 
         $crawler = $client->submitForm('Remove entry', [
-            'entry_remove_form[identifier]' => '123-test',
+            'entry_remove_form[identifier]' => '123-hest',
         ]);
         $this->assertResponseIsUnprocessable();
         $this->assertSelectorTextContains('*', 'Invalid identifier');
 
         $client->submitForm('Remove entry', [
-            // 'entry_remove_form[identifier]' => '123',
-            $this->getFormFieldNameByLabel($crawler, 'Identifier') => '123',
+            $this->getFormFieldNameByLabel('Identifier') => '123',
             // @todo Test with invalid value (error: "The selected choice is invalid")
             // 'entry_remove_form[department]' => 'xxx',
         ]);
@@ -63,24 +63,5 @@ class AppTest extends WebTestCase
 
         $client->followRedirect();
         $this->assertSelectorTextContains('h1', 'Entry removed');
-    }
-
-    public function getFormFieldNameByLabel(Crawler $crawler, string $value): string
-    {
-        $labels = $crawler->filterXPath(
-            \sprintf('descendant-or-self::label[contains(concat(\' \', normalize-space(string(.)), \' \'), %1$s)]', Crawler::xpathLiteral(' '.$value.' '))
-        );
-        if (1 !== $labels->count()) {
-            throw new InvalidArgumentException(\sprintf('There is no label with "%s" as its content.', $value));
-        }
-        $for = $labels->attr('for');
-        if ('' !== $for) {
-            $fields = $crawler->filter('[id="'.$for.'"]');
-            if ($fields->count() > 0) {
-                return $fields->attr('name');
-            }
-        }
-
-        throw new InvalidArgumentException(\sprintf('There is no field with ID "%s" for label "%s"', $for, $value));
     }
 }
--- tests/E2E/PantherTestCase.php
+++ tests/E2E/PantherTestCase.php
@@ -2,10 +2,8 @@
 
 namespace App\Tests\E2E;
 
-use App\Repository\UserRepository;
+use App\Tests\ApplicationTestCaseTrait;
 use Symfony\Component\BrowserKit\Cookie;
-use Symfony\Component\BrowserKit\Exception\InvalidArgumentException;
-use Symfony\Component\DomCrawler\Crawler;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\HttpFoundation\Session\Storage\MockFileSessionStorage;
 use Symfony\Component\Panther\Client;
@@ -15,6 +13,8 @@ use Symfony\Component\Security\Http\Authenticator\Token\PostAuthenticationToken;
 
 class PantherTestCase extends BasePantherTestCase
 {
+    use ApplicationTestCaseTrait;
+
     // https://github.com/symfony/panther/issues/648
     protected static function authenticateClient(Client $client, UserInterface $user, string $firewallName = 'main'): Client
     {
@@ -30,63 +30,5 @@ class PantherTestCase extends BasePantherTestCase
         $client->getCookieJar()->set($cookie);
 
         return $client;
-    }
-
-    protected static function getUser(string $email): UserInterface
-    {
-        /** @var UserRepository $userRepository */
-        $userRepository = static::getContainer()->get(UserRepository::class);
-        $user = $userRepository->findOneByEmail($email);
-
-        if (null === $user) {
-            throw new \RuntimeException(sprintf('User with email "%s" not found.', $email));
-        }
-
-        return $user;
-    }
-
-    protected function formValuesByLabel(array $values): array
-    {
-        $byName = [];
-
-        foreach ($values as $label => $value) {
-            $byName[$this->getFormFieldNameByLabel($label)] = $value;
-        }
-
-        return $byName;
-    }
-
-    public function getFormFieldNameByLabel(string $label): string
-    {
-        $crawler = static::getClient()->getCrawler();
-        $labels = $crawler->filterXPath(
-            \sprintf('descendant-or-self::label[contains(concat(\' \', normalize-space(string(.)), \' \'), %1$s)]', Crawler::xpathLiteral(' '.$label.' '))
-        );
-        if (1 !== $labels->count()) {
-            throw new InvalidArgumentException(\sprintf('There is no label with "%s" as its content.', $label));
-        }
-        $for = $labels->attr('for');
-        if ('' !== $for) {
-            $fields = $crawler->filter('[id="'.$for.'"]');
-            if ($fields->count() > 0) {
-                return $fields->attr('name');
-            }
-        }
-
-        throw new InvalidArgumentException(\sprintf('There is no field with ID "%s" for label "%s"', $for, $label));
-    }
-
-    // Lifted from https://github.com/coccoinomane/phpunit-log
-    protected static function print(mixed $message): void
-    {
-        $stream = STDERR;
-
-        if (is_array($message) || is_object($message)) {
-            fwrite($stream, print_r($message, true));
-        } else {
-            fwrite($stream, $message);
-        }
-
-        fwrite($stream, PHP_EOL);
     }
 }
